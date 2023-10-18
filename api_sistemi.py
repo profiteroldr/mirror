b334cc7811ab625f601d802a99490de4aecd1adc
koncayusuf <45740188+yusufkonca@users.noreply.github.com>
2023-10-18 15:22:14 +0300

    Delete api_sistemi.py

--- api_sistemi.py
+++
@@ -1,5 +0,0 @@
-sehir_ismi = "Ankara"
-
-api = "89f43f1f20b6e6c6e3a602d47906405b"
-base_url = "https://api.openweathermap.org/data/2.5/weather?"
-url = base_url + "appid=" + api + "&q=" + sehir_ismi
