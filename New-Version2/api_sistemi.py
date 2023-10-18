3b39efb54174fe755aa1cce33a4e76c95a5ca9a9
koncayusuf <45740188+yusufkonca@users.noreply.github.com>
2023-10-18 15:23:23 +0300

    Add files via upload

---
+++ New-Version2/api_sistemi.py
@@ -0,0 +1,5 @@
+sehir_ismi = "Ankara"
+
+api = "89f43f1f20b6e6c6e3a602d47906405b"
+base_url = "https://api.openweathermap.org/data/2.5/weather?"
+url = base_url + "appid=" + api + "&q=" + sehir_ismi
